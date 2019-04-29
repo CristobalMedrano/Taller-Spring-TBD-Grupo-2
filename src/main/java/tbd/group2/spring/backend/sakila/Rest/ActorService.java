a59d7ef59d8678e11395362e897321e7ac11d9c4
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2019-04-29 00:56:18 -0400

    Refactor completo, Gradle bootrun corregido
    Refactor en Actor terminado.
    
    Se agregaron las respuestas de error http y se validaron todas las entradas.
    Refator en Film terminado.
    
    Se corrige la hora en el update de Actor
    Se actualizan las validaciones en Film

--- src/main/java/tbd/group2/spring/backend/sakila/Rest/ActorService.java
+++ src/main/java/tbd/group2/spring/backend/sakila/Rest/ActorService.java
@@ -4,14 +4,17 @@ import org.hibernate.annotations.SortNatural;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tbd.group2.spring.backend.sakila.Entities.Actor;
 import tbd.group2.spring.backend.sakila.Entities.Film;
 import tbd.group2.spring.backend.sakila.Repository.ActorRepository;
 import tbd.group2.spring.backend.sakila.Repository.FilmRepository;
 
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.*;
-@CrossOrigin(origins = "Http://localhost:8080")
+@CrossOrigin
 @RestController
 @RequestMapping("/actors")
 public class ActorService {
@@ -29,91 +32,129 @@ public class ActorService {
 
     /* CREATE */
     @RequestMapping(value = "/create", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void create(@RequestBody Actor actor)
+    public ResponseEntity create(@RequestBody Actor actor)
     {
         String firstName = actor.getFirstName();
         String lastName = actor.getLastName();
-        if (null == actorRepository.findActorByFirstNameAndLastName(firstName, lastName))
+        if (firstName == null || lastName == null)
         {
-            System.out.println("Actor "+firstName+" "+lastName+" saved in sakila-db");
-            actorRepository.save(actor);
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("insufficient actor information.");
         }
         else
-        {
-            System.out.println("Actor "+firstName+" "+lastName+" already in sakila-db");
-        }
+            {
+                if (null == actorRepository.findActorByFirstNameAndLastName(firstName, lastName))
+                {
+                    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                    actor.setLastUpdate(timestamp);
+                    actorRepository.save(actor);
+                    System.out.println("Actor "+firstName+" "+lastName+" saved in sakila-db");
+                    return ResponseEntity.ok("Actor "+firstName+" "+lastName+" saved in sakila-db");
+                }
+                else
+                {
+                    System.out.println("Actor "+firstName+" "+lastName+" already in sakila-db");
+                    return ResponseEntity.status(HttpStatus.CONFLICT).body("Actor "+firstName+" "+lastName+" already in sakila-db");
+                }
+            }
+
     }
 
     /* SHOW (READ) */
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Actor show(@PathVariable Integer id)
+    public ResponseEntity<Actor> show(@PathVariable Integer id)
     {
-        return this.actorRepository.findActorByActorId(id);
+        Actor actor = actorRepository.findActorByActorId(id);
+        if (null == actor)
+        {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(null);
+        }
+        else
+            {
+                return ResponseEntity.ok(actor);
+            }
     }
 
     /* UPDATE */
     @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void update(@PathVariable ("id") Integer id, @RequestBody Actor actor)
+    public ResponseEntity update(@PathVariable ("id") Integer id, @RequestBody Actor actor)
     {
-        String firstName = actor.getFirstName();
-        String lastName = actor.getLastName();
-        if (null == actorRepository.findActorByFirstNameAndLastName(firstName, lastName))
+        if (null == actorRepository.findActorByActorId(id))
         {
-
-            Actor updatedActor = actorRepository.findActorByActorId(id);
-            updatedActor.setFirstName(actor.getFirstName());
-            updatedActor.setLastName(actor.getLastName());
-            updatedActor.setLastUpdate(actor.getLastUpdate());
-            System.out.println("Actor "+updatedActor.getFirstName()+" "+updatedActor.getLastName()+" updated");
-            this.actorRepository.save(updatedActor);
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("Actor not found.");
         }
+        else
+            {
+                String firstName = actor.getFirstName();
+                String lastName = actor.getLastName();
+                if (null == actorRepository.findActorByFirstNameAndLastName(firstName, lastName))
+                {
+                    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                    Actor updatedActor = actorRepository.findActorByActorId(id);
+                    updatedActor.setFirstName(actor.getFirstName());
+                    updatedActor.setLastName(actor.getLastName());
+                    updatedActor.setLastUpdate(timestamp);
+                    System.out.println("Actor "+updatedActor.getFirstName()+" "+updatedActor.getLastName()+" updated");
+                    this.actorRepository.save(updatedActor);
+                    return ResponseEntity.ok("Actor "+updatedActor.getFirstName()+" "+updatedActor.getLastName()+" updated");
+                }
+                else
+                    {
+                        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Actor already added.");
+                    }
+            }
     }
 
     /* DELETE */
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable ("id") Integer id)
+    public ResponseEntity delete(@PathVariable ("id") Integer id)
     {
         Actor actor = actorRepository.findActorByActorId(id);
         if (null == actor)
         {
             System.out.println("Actor not found");
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body("Actor not found");
         }
         else
         {
             System.out.println("Actor id: "+id+" deleted");
             actorRepository.delete(actor);
+            return ResponseEntity.ok("Actor id: "+id+" deleted");
         }
 
     }
-    /* ENCONTRAR PELICULAS POR ACTOR */
+
+    /* FIND FILMS BY ACTOR */
     @SortNatural
     @RequestMapping(value = "/{id}/films", method = RequestMethod.GET)
-    public List<Film> obtenerPeliculasDeActor (@PathVariable("id") Integer id) {
-
-        Set<Film> films = actorRepository.findActorByActorId(id).getFilms();
-        List<Film> sortedFilms = new ArrayList<>(films);
-        Collections.sort(sortedFilms, (Film f1, Film f2) -> f1.getFilmId()-f2.getFilmId());
-        return sortedFilms;
+    public ResponseEntity<List> getFilmsByActor(@PathVariable("id") Integer id)
+    {
+        if (null != actorRepository.findActorByActorId(id))
+        {
+            Set<Film> films = actorRepository.findActorByActorId(id).getFilms();
+            List<Film> sortedFilms = new ArrayList<>(films);
+            Collections.sort(sortedFilms, (Film f1, Film f2) -> f1.getFilmId()-f2.getFilmId());
+            return ResponseEntity.ok(sortedFilms);
+        }
+        else
+        {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(null);
+        }
     }
 
-    /* RELACIONAR ACTOR CON PELICULA */
-    @PostMapping("/{idA}/films/{idP}")
+    /* SAVE AN ACTOR WITH FILM */
+    @PostMapping("/{idA}/films/{idF}")
     @ResponseBody
-    public  String match (@PathVariable("idA") Integer idA, @PathVariable("idP") Integer idP) {
+    public ResponseEntity match (@PathVariable("idA") Integer idA, @PathVariable("idF") Integer idF) {
 
         Actor actor = actorRepository.findActorByActorId(idA);
-        Film film = filmRepository.findFilmByFilmId(idP);
-        System.out.println(":D");
-        if ( actor !=  null &&  film != null) {
+        Film film = filmRepository.findFilmByFilmId(idF);
+        if (actor != null && film != null) {
             actor.getFilms().add(film);
             film.getActors().add(actor);
             actorRepository.save(actor);
-            System.out.println(":D");
-            return "OK";
+            return ResponseEntity.ok("Actor id: "+idA+" with Film id: "+idF+" saved.");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(null);
         }
-        else
-            System.out.println("D:");
-            return "MALO";
     }
-
 }
--- src/main/java/tbd/group2/spring/backend/sakila/Rest/FilmService.java
+++ src/main/java/tbd/group2/spring/backend/sakila/Rest/FilmService.java
@@ -2,6 +2,8 @@ package tbd.group2.spring.backend.sakila.Rest;
 
 import org.hibernate.annotations.SortNatural;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import tbd.group2.spring.backend.sakila.Entities.Actor;
 import tbd.group2.spring.backend.sakila.Entities.Film;
@@ -13,72 +15,70 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
-@CrossOrigin(origins = "Http://localhost:8080")
+@CrossOrigin
 @RestController
 @RequestMapping("/films")
 public class FilmService {
+
     @Autowired
     private FilmRepository filmRepository;
+
     @Autowired
     private ActorRepository actorRepository;
-    @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
-    public Iterable<Film> getAllUsers() {
+
+    /* INDEX */
+    @GetMapping("")
+    public List<Film> index(){
         return filmRepository.findAll();
     }
 
+    /* SHOW (READ) */
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public  Film findOne(@PathVariable("id") Integer id) {
-        return filmRepository.findFilmByFilmId(id);
+    public ResponseEntity<Film> show(@PathVariable Integer id)
+    {
+        Film film = filmRepository.findFilmByFilmId(id);
+        if (null == film)
+        {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(null);
+        }
+        else
+        {
+            return ResponseEntity.ok(film);
+        }
     }
 
-    /* OBTENER ACTORES DESDE PELICULA */
-    /* ENCONTRAR PELICULAS POR ACTOR */
+    /* FIND ACTORS BY FILM */
     @SortNatural
     @RequestMapping(value = "/{id}/actors", method = RequestMethod.GET)
-    public List<Actor> obtenerActoresDePeliculas (@PathVariable("id") Integer id) {
-
-        Set<Actor> actors = filmRepository.findFilmByFilmId(id).getActors();
-        List<Actor> sortedActors = new ArrayList<>(actors);
-        Collections.sort(sortedActors, (Actor a1, Actor a2) -> a1.getActorId()-a2.getActorId());
-        return sortedActors;
+    public ResponseEntity<List> getActorsByFilm(@PathVariable("id") Integer id)
+    {
+        if (null != filmRepository.findFilmByFilmId(id))
+        {
+            Set<Actor> actors = filmRepository.findFilmByFilmId(id).getActors();
+            List<Actor> sortedActors = new ArrayList<>(actors);
+            Collections.sort(sortedActors, (Actor a1, Actor a2) -> a1.getActorId()-a2.getActorId());
+            return ResponseEntity.ok(sortedActors);
+        }
+        else
+        {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(null);
+        }
     }
-    @PostMapping("/{idP}/actors/{idA}")
+
+    /* SAVE A FILM WITH ACTOR */
+    @PostMapping("/{idF}/actors/{idA}")
     @ResponseBody
-    public  String match (@PathVariable("idA") Integer idA, @PathVariable("idP") Integer idP) {
+    public ResponseEntity match (@PathVariable("idF") Integer idF, @PathVariable("idA") Integer idA) {
+
         Actor actor = actorRepository.findActorByActorId(idA);
-        Film film = filmRepository.findFilmByFilmId(idP);
-        System.out.println(":D");
-        if ( actor !=  null &&  film != null) {
+        Film film = filmRepository.findFilmByFilmId(idF);
+        if (actor != null && film != null) {
             actor.getFilms().add(film);
             film.getActors().add(actor);
             actorRepository.save(actor);
-            System.out.println(":D");
-            return "OK";
+            return ResponseEntity.ok("Films id: "+idF+" with Actor id: "+idA+" saved.");
+        } else {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(null);
         }
-        else
-            System.out.println("D:");
-        return "MALO";
     }
-    /*@RequestMapping(value = "/test/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public Film findFilm(@PathVariable("id") Integer id){
-        return filmRepository.encontrarPelicula(id);
-    }
-
-
-
-    @RequestMapping(value = "/{id}/actors", method = RequestMethod.GET)
-    @ResponseBody
-    public List<Actor> getActorsFilm(@PathVariable("id") Integer id){
-        Iterable<Integer> actors_ids = filmActorRepository.encontrarActorDesdeIdPelicula(id);
-        List<Actor> actors = new ArrayList();
-        for (Integer i: actors_ids
-        ) {
-
-            actors.add(actorRepository.findOne(i));
-        }
-        return actors;
-    }*/
 }
